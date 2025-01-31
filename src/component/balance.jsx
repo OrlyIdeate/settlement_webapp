7a03e7cb8fe9504a85799aebf275bbe1e188281a
OrlyIdeate <ueno.jin25@outlook.jp>
2025-01-31 19:04:05 +0900

    収支の表示に色を追加し、収入の表示形式を改善

--- src/component/balance.jsx
+++ src/component/balance.jsx
@@ -3,10 +3,12 @@ import { DataContext } from "../dataContext";
 
 export default function Balance() {
     const { balance } = useContext(DataContext);
+    const color = balance > 0 ? '#2ecc71' : balance < 0 ? '#e74c3c' : '#000';
 
     return (
         <div>
-            <h2>収支: { balance } 円</h2>
+            <h2 style={{ color: color }}>収支: { balance } 円</h2>
+            {/* <h2 className={balance > 0 ? '#2ecc71' : '#e74c3c'}>収支: { balance } 円</h2> */}
         </div>
     );
     }
--- src/component/income.jsx
+++ src/component/income.jsx
@@ -3,10 +3,11 @@ import { DataContext } from "../dataContext";
 
 export default function Income() {
     const { income } = useContext(DataContext);
+    const plus  = income == 0 ? null : "+";
 
     return (
-        <div className="col">
-            <h2 className="income">収入: +{ income } 円</h2>
+        <div className='col'>
+            <h2 className="income">収入: { plus + income } 円</h2>
         </div>
     )
 }
